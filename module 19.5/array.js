1040e7cb7ec163d3f5678825bb3896e825733a83
Fatema Tuz Johora <138400099+fatihatuzjohora@users.noreply.github.com>
2024-02-14 19:09:56 +0600

    Add files via upload
    first done

---
+++ module 19.5/array.js
@@ -0,0 +1,28 @@
+const friends=['ema', 'rima', 'sima', 'jima'];
+for (const friend of friends){
+// console.log(friend);
+}
+
+for(let i=0; i<friends.length; i++){
+  //  console.log(friends[i]);
+}
+
+let i =0;
+while(i<friends.length){
+  //  console.log(friends[i]);
+    i++;
+}
+
+//----- array reverse----
+const number=[1,2,3,4,5,6,7,8,9]
+// console.log(number);
+const N=number.reverse();
+// console.log(N);
+
+
+//---------sort-----
+// ascending:smale to large. 
+// descending:large to small.
+const person=['rakib', 'sakib', 'nakib'];
+const persons=person.sort();
+console.log(persons);
